0507e48b55424921826f8067bfeda4103545bea9
Ben Mohammad <benmohammad593@gmail.com>
2020-08-29 06:55:19 +0100

    Initial commit

---
+++ app/src/main/java/com/benmohammad/rearkapp/view/RepositoriesView.java
@@ -0,0 +1,18 @@
+package com.benmohammad.rearkapp.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import org.w3c.dom.Attr;
+
+public class RepositoriesView extends FrameLayout {
+
+    public RepositoriesView(Context context) {
+        super(context);
+    }
+
+    public RepositoriesView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/benmohammad/rearkapp/view/RepositoryView.java
@@ -0,0 +1,17 @@
+package com.benmohammad.rearkapp.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class RepositoryView extends FrameLayout {
+
+    public RepositoryView(Context context) {
+        super(context);
+    }
+
+    public RepositoryView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
